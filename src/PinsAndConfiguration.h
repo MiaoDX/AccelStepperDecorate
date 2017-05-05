75ce11199c03db5caf681048b9f6bf06111c5c6c
miaodx <miaodx@hotmail.com>
2017-05-05 15:42:57 +0800

    Reset the pins to RAMPS 1.4

--- src/PinsAndConfiguration.h
+++ src/PinsAndConfiguration.h
@@ -6,25 +6,15 @@
 
 
 // For RAMPS 1.4
-//#define X_STEP_PIN         54
-//#define X_DIR_PIN          55
-//#define X_ENABLE_PIN       38
-
-#define X_STEP_PIN         12
-#define X_DIR_PIN          11
-#define X_ENABLE_PIN       13
-
+#define X_STEP_PIN         54
+#define X_DIR_PIN          55
+#define X_ENABLE_PIN       38
 #define X_MIN_PIN           3
 #define X_MAX_PIN           2
 
-//#define Y_STEP_PIN         60
-//#define Y_DIR_PIN          61
-//#define Y_ENABLE_PIN       56
-
-#define Y_STEP_PIN         3
-#define Y_DIR_PIN          2
-#define Y_ENABLE_PIN       4
-
+#define Y_STEP_PIN         60
+#define Y_DIR_PIN          61
+#define Y_ENABLE_PIN       56
 #define Y_MIN_PIN          14
 #define Y_MAX_PIN          15
 
